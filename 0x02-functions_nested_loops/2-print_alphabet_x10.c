24aa1c49ccfbe62026ecde8c9d9632ba25edfa27
MercyMueniMwangi <mercymwangi1996@gmail.com>
2020-09-23 22:27:55 +0000

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,22 @@
 #include "holberton.h"
+
 /**
- *main - pritnts alphabets 10 times
+ *main - Entry point
  *
- *Return:0 
+ *Description: prints 10 times the alphabet, in lowercase, followed by a new line.
+ *@c: char type letter
+ *Return: alphabets * 10
  */
 
 void print_alphabet_x10(void)
 {
 	char c;
-	int a =0;
-	while(a < 10)
+	int a = 0;
+
+	while (a < 10)
 	{
 		c = 'a';
-		while(c<= 'z')
+		while (c <= 'z')
 		{
 		_putchar(c);
 		c++;
