f3aee9924ec6294a6d70cea6d73b211a810b4901
MercyMueniMwangi <mercymwangi1996@gmail.com>
2020-09-24 20:14:56 +0000

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -86,4 +86,17 @@ void print_square(int size);
 
 void print_triangle(int size);
 
+/**
+ * main - print largest prime factor of 612852475143
+ * Return: 0 on success
+ */
+
+int main(void);
+
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n);
 #endif
