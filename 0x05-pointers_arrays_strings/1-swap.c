5934f4d3bf0b5fe91c831006b7da8e799055e139
MercyMueniMwangi <mercymwangi1996@gmail.com>
2020-10-13 19:09:32 +0000

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a - value 1
+ * @b - value 2
+ *
+ *Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ *@s - string
+ *
+ *Return: 0 always
+ */
+
+int _strlen(char *s)
+{
+
+	int length;
+
+	for(length = 0; s[length] != '\0'; length++);
+	return (length);
+}
