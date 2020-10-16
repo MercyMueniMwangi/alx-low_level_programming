8c652680278087c345c8dcda32801a510eaf6916
MercyMueniMwangi <mercymwangi1996@gmail.com>
2020-10-16 13:46:00 +0000

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,21 +1,61 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_number -  print an integer, without using long, arrays, or pointers.
- * @n: The integer to be printed.
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
  */
-void print_number(int n)
+
+void print_line(char *c, int s, int l)
 {
-	unsigned int num = n;
+	int j, k;
 
-	if (n < 0)
+	for (j = 0; j <= 9; j++)
 	{
-		_putchar('-');
-		num = -num;
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
 	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
 
-	_putchar((num % 10) + '0');
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
